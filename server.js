cc3045b112e7cb751c2703a413f64c8e1d103787
tejesh767 <95077484+tejesh767@users.noreply.github.com>
2024-08-01 19:53:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/videos/:id', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 10000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
